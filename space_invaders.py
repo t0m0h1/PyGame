23fdc4a30d76200846e40e317d6c7a7b03ce6d6e
t0m0h1 <thomashathawayhogg@gmail.com>
2024-01-13 19:05:52 +0000

    rebase

--- space_invaders.py
+++ space_invaders.py
@@ -1,6 +1,17 @@
 import pygame
 import sys
 
+# writing game class
+class Game:
+    def __init__(self):
+        pass
+
+    def run_game_loop(self):
+        pass
+
+
+# initializing pygame
+
 pygame.init()
 swidth, sheight = 500, 500
 screen = pygame.display.set_mode((swidth, sheight))

aad8258ecc2659765c1d42c7626b39ad60e5c073
t0m0h1 <thomashathawayhogg@gmail.com>
2024-01-13 22:45:07 +0000

    laser settings

--- laser.py
+++ laser.py
@@ -1,12 +1,12 @@
 import pygame
 
-yellow = (255, 255, 0)
+light_yellow = (255, 255, 102)
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((5, 10))
-        self.image.fill(yellow)
+        self.image = pygame.Surface((3, 15))
+        self.image.fill(light_yellow)
         self.rect = self.image.get_rect(center=pos)
 
 

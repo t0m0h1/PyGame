10b77bc6f0d52853bcdb56e85a92b30202860374
t0m0h1 <thomashathawayhogg@gmail.com>
2024-01-13 20:07:32 +0000

    change image position

--- space_invaders.py
+++ space_invaders.py
@@ -6,7 +6,7 @@ from player import Player # importing player class from player.py
 class Game:
     def __init__(self, screen):  # Add the 'screen' parameter
         self.screen = screen
-        player_sprite = Player((300, 600))
+        player_sprite = Player((250, 430))
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
     def run(self):

1c047f6091036f53c556630da9ebfdd65c5dc7a1
t0m0h1 <thomashathawayhogg@gmail.com>
2024-01-13 19:08:36 +0000

    game window and loop

--- space_invaders.py
+++ space_invaders.py
@@ -11,21 +11,21 @@ class Game:
 
 
 # initializing pygame
+if __name__ == "__main__":
+    pygame.init()
+    swidth, sheight = 500, 500
+    screen = pygame.display.set_mode((swidth, sheight))
+    pygame.display.set_caption("Space Invaders")
+    clock = pygame.time.Clock()
 
-pygame.init()
-swidth, sheight = 500, 500
-screen = pygame.display.set_mode((swidth, sheight))
-pygame.display.set_caption("Space Invaders")
-clock = pygame.time.Clock()
+    #Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-#Main game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        screen.fill((0,0,0))
 
-    screen.fill((0,0,0))
-
-    pygame.display.flip()
-    clock.tick(60)
+        pygame.display.flip()
+        clock.tick(60)

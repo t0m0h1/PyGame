51572051b237cf37e4db66fcde359aa424c0ea34
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-01-16 22:28:04 +0000

    changes

--- snake/main.py
+++ snake/main.py
@@ -22,7 +22,37 @@ clock = pygame.time.Clock()
 snake_block = 10
 snake_speed = 15
 
+message_font = pygame.font.SysFont("ubuntu", 35)
 game_font = pygame.font.SysFont("comicsansms", 35)
+
+# function to display score
+def display_score(score):
+    value = game_font.render("Your Score: " + str(score), True, orange)
+    screen.blit(value, [0, 0])
+
+# function to display snake
+def draw_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, green, [x[0], x[1], snake_block, snake_block])
+
+def run():
+    game_over = False
+    game_close = False
+
+    # initial position of snake
+    x, y = swidth / 2, sheight / 2
+
+    # initial velocity of snake
+    x_change, y_change = 0, 0
+
+    # snake body
+    snake_pixels = []
+    snake_length = 1
+
+    # food position
+    food_x = round(random.randrange(0, swidth - snake_block) / 10.0) * 10.0
+
+
 
 # Game loop
 running = True
@@ -32,7 +62,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    
+
     # Game logic
+    
 
     # Drawing code
     screen.fill((0, 0, 0))  # Fill the screen with black color

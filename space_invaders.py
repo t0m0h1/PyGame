e7dc7fdcbaba01efd4f52e710995ec23125213ce
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-01-16 14:59:16 +0000

    obstacle positions

--- space_invaders.py
+++ space_invaders.py
@@ -21,12 +21,12 @@ class Game:
         self.obstacle_amount = 4
         self.obstacle_x_positions = [num * (self.screen.get_width() // (self.obstacle_amount + 1)) for num in range(1, self.obstacle_amount + 1)]
         # calling create_obstacle method
-        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start=0 / 15, y_start=480)
+        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start=screen.get_width() / 15, y_start=480)
 
     def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
-                if col == x:
+                if col == 'x':
                     x = x_start + col_index * self.block_size + offset_x
                     y = y_start + row_index * self.block_size 
                     block = obstacle.Block(self.block_size, (241, 79, 80), x, y)

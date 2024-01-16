68076bcefa0bc13b179111e45e3d1f7023897b85
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-01-16 15:24:47 +0000

    alien class

---
+++ alien.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.sprite import _Group
+
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load('images/alien.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 5

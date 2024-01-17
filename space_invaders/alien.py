905593ccea963579fe057b7365db6fa65079d527
t0m0h1 <thomashathawayhogg@gmail.com>
2024-01-17 22:03:51 +0000

    changes

--- space_invaders/alien.py
+++ space_invaders/alien.py
@@ -4,7 +4,7 @@ import pygame
 class Alien(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load('images/alien.png')
+        self.image = pygame.image.load("assets/alien.png")
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
